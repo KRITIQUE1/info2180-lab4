11acf1fc19b1677947b5c069dca7256286472ea2
KRITIQUE1 <triple_robinson@hotmail.com>
2016-10-14 23:47:41 -0400

    "Exercise 1"
    first wall red

---
+++ maze.js
@@ -0,0 +1,11 @@
+
+window.onload= function() {
+    document.getElementById("boundary1").addEventListener("mouseover",function(){
+
+        this.className += " youlose"; 
+        //new thing
+    });
+ };
+    
+    
+
