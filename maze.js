5f469de69200e350c8c01786760ea34439e9bec4
KRITIQUE1 <triple_robinson@hotmail.com>
2016-10-15 00:30:58 -0400

    Exercise 3
    Alerting

--- maze.js
+++ maze.js
@@ -1,15 +1,32 @@
 
+
 window.onload= function() {
     var booundary = document.getElementsByClassName("boundary");
+    var winner = true;
+    var end = document.getElementById("end");
     
     for (var i = 0; i < booundary.length-1; i++){
      booundary[i].addEventListener("mouseover",function(){
-         for (var j = 0 ; j < booundary.length-1; j++){
+        for (var j = 0 ; j < booundary.length-1; j++){
         booundary[j].className += " youlose"; 
+        winner=false;
          }
+         
     });
     }
+    end.addEventListener("mouseover",function(){
+        if (winner == false){
+            alert("Sorry, But you lose!");
+        }
+        else if (winner == true){
+            alert("ERMMMAH GOD You won!");
+        }
+    });
+    
+
+    
  };
     
     
+
 
