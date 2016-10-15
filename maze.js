37456cb307b0acc6c89402c110c40abb555bcbe7
KRITIQUE1 <triple_robinson@hotmail.com>
2016-10-15 00:05:49 -0400

    Exercise 2
    All walls turn red

--- maze.js
+++ maze.js
@@ -1,10 +1,14 @@
 
 window.onload= function() {
-    document.getElementById("boundary1").addEventListener("mouseover",function(){
-
-        this.className += " youlose"; 
-        //new thing
+    var booundary = document.getElementsByClassName("boundary");
+    
+    for (var i = 0; i < booundary.length-1; i++){
+     booundary[i].addEventListener("mouseover",function(){
+         for (var j = 0 ; j < booundary.length-1; j++){
+        booundary[j].className += " youlose"; 
+         }
     });
+    }
  };
     
     
