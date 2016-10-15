7f5343d92c07a562fd5c0366919fdf0d51aeacce
KRITIQUE1 <triple_robinson@hotmail.com>
2016-10-15 00:49:17 -0400

    Exercise 5
    Staus

--- maze.js
+++ maze.js
@@ -17,23 +17,27 @@ window.onload= function() {
     end.addEventListener("mouseover",function(){
         if (winner == false){
             alert("Sorry, But you lose!");
+            document.getElementById("status").innerHTML = "Sorry, You Lose";
         }
         else if (winner == true){
             alert("ERMMMAH GOD You won!");
+             document.getElementById("status").innerHTML = "Wow, You Won";
         }
     });
     
     restart.addEventListener("click", function(){
-        for(var y = 0; y < booundary.length-1; y++){
-            for(var z = 0; 0<booundary.length-1;z++){
+        for(var y = 0; y < (booundary.length-1); y++){
+            for(var z = 0; 0 < (booundary.length-1);z++){
                 winner = true;
                 booundary[z].className += "boundary";
             }
         }
+         document.getElementById("status").innerHTML = "Move to S to begin";
     });
 
     
  };
     
     
+
 
